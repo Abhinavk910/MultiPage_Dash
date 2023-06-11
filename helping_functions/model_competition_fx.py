87b2800adf1a2e8faf3f6093fe5be1e44659fe60
abhinav <abhinavk910@gmail.com>
2023-06-11 10:40:45 +0530

    dash8

---
+++ helping_functions/model_competition_fx.py
@@ -0,0 +1,212 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 11 00:57:01 2023
+
+@author: abhinav.kumar
+"""
+
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import AI21
+from langchain.schema import HumanMessage
+from langchain.llms import Replicate
+
+model_data = [
+        'OpenAI - gpt-3.5-turbo',
+        'OpenAI - gpt-3.5-turbo-0301', 
+        'OpenAI - text-davinci-003',
+        'OpenAI - text-davinci-002',
+        'AI21 - j1-large',
+        'AI21 - j1-grande',
+        'AI21 - j1-jumbo',
+        'AI21 - j2-large',
+        'AI21 - j2-grande',
+        'AI21 - j2-grande-instruct',
+        'AI21 - j2-jumbo',
+        'AI21 - j2-jumbo-instruct',
+        'Replicate - vicuna-13b',
+        'Replicate - stability-ai/stablelm-tuned-alpha-7b',
+        'Replicate - llama-7b',
+        'Replicate - dolly-v2-12b',
+        'Replicate - oasst-sft-1-pythia-12b',
+        'Replicate - gpt-j-6b'
+]
+
+
+#helping Functions
+def get_model_grid(i, id1, id2):
+    """
+    function create two single select dropdown
+    one is for llm name , second is temp
+    """
+    return dmc.Grid(
+        align='center',
+        justify='space-around',
+        style={'width':'90%'},
+        mb=5,
+        children=[
+            dmc.Col(
+                span=6,
+                sm=4,
+                children=[
+                    dmc.Select(
+                        id=id1,
+                        label='Models',
+                        description='Select atleast one',
+                        data=model_data,
+                        searchable=True,
+                        required=True,
+                        persistence=id1,
+                        persistence_type='session'
+                    )
+                ]
+            ),
+            dmc.Col(
+                span=6,
+                sm=4,
+                children=[
+                    dmc.NumberInput(
+                        id=id2,
+                        label="Temperature",
+                        description='Setting temperature to 0 will make the outputs mostly deterministic',
+                        value=0.5,
+                        precision=1,
+                        min=0,
+                        step=0.1,
+                        max=1,
+                    ) 
+                ]
+            )
+        ]
+    )
+        
+        
+def create_acccordion_item(value, control_name, password_id, button_id, alert1_id, alert2_id, api_description=''):
+    return dmc.AccordionItem(
+                value=value,
+                children=[
+                    dmc.AccordionControl(control_name),
+                    dmc.AccordionPanel(
+                        children=[
+                            dmc.Grid(
+                                align='center',
+                                children=[
+                                    dmc.Col(
+                                        span=12,
+                                        sm=6,
+                                        children=[
+                                            dmc.PasswordInput(
+                                                id=password_id,
+                                                label="API Key",
+                                                description=api_description,
+                                                placeholder="Your Key",
+                                                # style={"width": 250},
+                                                icon=DashIconify(icon="material-symbols:key-outline"),
+                                            ),
+                                        ]
+                                    ),
+                                    dmc.Col(
+                                        span=12,
+                                        sm=6,
+                                        children=[
+                                            dmc.Center(
+                                                mt=14,
+                                                children=[
+                                                    dmc.Button('Register',
+                                                        id=button_id,
+                                                        variant='gradient',
+                                                        leftIcon=DashIconify(icon="pajamas:api"),
+                                                        style={'textAlign':'center'},
+                                                    )
+                                                ]
+                                            )
+                                        ]
+                                    )
+                                ]
+                            ),
+                            dmc.Alert(
+                                id=alert1_id,
+                                children=['API key registered!!!'],
+                                title="Success!",
+                                color="green",
+                                hide=True,
+                                duration=10000
+                            ),
+                            dmc.Alert(
+                                id=alert2_id,
+                                children=['API key not registered. Problem in Key'],
+                                title="Failure",
+                                color="red",
+                                hide=True,
+                                duration=10000
+                            ),
+                        ]
+                    )
+                ]
+            )
+
+
+def create_card_p8(i, model, temp, id1, id2, id3, id4):
+    return dmc.Col(
+            span=12,
+            sm=6,
+            md=4,
+            children=[
+                dmc.Card(
+                    id=id3,
+                    withBorder=True,
+                    shadow="sm",
+                    radius="md",
+                    style={"height": '100%'},
+                    children=[
+                        dmc.CardSection(
+                            dmc.Title(model+ " - " +str(temp), order=2, p=15, style={'height':'100px'}),
+                        ),
+                        dmc.Group(
+                            [
+                                dmc.Badge(" ", color="grey", variant="light", id=id4),
+                                dmc.Badge(" ", color="grey", variant="light", id=id1),
+                            ],
+                            position="apart",
+                            mt="md",
+                            mb="xs",
+                        ),
+                        dmc.Textarea(
+                            id=id2,
+                            size="sm",
+                            variant="filled",
+                            placeholder='Answer',
+                            autosize=True,
+                            minRows=6,
+                        ),
+                    ]
+                )
+            ]
+        )
+
+
+def get_answer_openai(question, model, api_key, temp):
+    if model == 'gpt-3.5-turbo' or model == 'gpt-3.5-turbo-0301':
+        llm = ChatOpenAI(model_name=model, openai_api_key=api_key, temperature=temp)
+        text = llm([HumanMessage(content=question)])
+        text = text.content
+        return text
+    else:
+        llm = OpenAI(model_name=model, openai_api_key=api_key, temperature=temp)
+        text = llm(question)
+        return text
+    
+    
+def get_answer_ai21(question, model, api_key, temp):
+    llm = AI21(model=model, ai21_api_key=api_key, temperature=temp)
+    text = llm(question)
+    return text
+
+
+def get_answer_replicate(question, model, api_key, temp):
+    llm = Replicate(model = model, model_kwargs=[{'temperature':temp, 'max_token':100}])
+    text = llm(question)
+    return text
---
+++ helping_pages/model_competition_layout.py
@@ -0,0 +1,251 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 11 01:02:11 2023
+
+@author: abhinav.kumar
+"""
+
+import dash_mantine_components as dmc
+from dash import html,  dcc
+from dash_iconify import DashIconify
+
+
+
+layout_mc = html.Div(
+    style={'backgroundColor':'#fff'},
+    children=[
+        dmc.Container(
+            id='conpetition-setup-p8',
+            style={'backgroundColor':'whitesmoke', 'minHeight':'100vh'},
+            px=0,
+            size='lg',
+            children=[
+                dcc.Interval(id='interval1-p8', interval=9*1000, n_intervals=0, max_intervals=1, disabled=False),
+                dcc.Store(id='competition-data-p8', data={'contender-count':0, 'keys':{},    
+                                                          'answered_question':0,
+                                                          'current_question':1}, storage_type='local'),
+                dmc.Stack(
+                    id='hero',
+                    align='center',
+                    children=[
+                        #one can also use dmc.header
+                        dmc.Paper(
+                            px=20,
+                            p=10,
+                            mt=0,
+                            w='100%',
+                            shadow="sm",
+                            mx=-2,
+                            style={"backgroundColor": '#f1faee'},
+                            children=[
+                                dmc.Group(
+                                    position='apart',
+                                    children=[
+                                        dmc.Text( id='header-text-p8', align='center', color='', weight=700),
+                                        dmc.Group(
+                                            spacing='xs',
+                                            position='right',
+                                            children=[
+                                                dmc.Text(
+                                                    align='center',
+                                                    color="#033546",
+                                                    weight=700,
+                                                    children=['Created By ',
+                                                          dmc.Anchor("Abhinav Kumar",href="http://www.linkedin.com/in/abhinavk910",
+                                                                target="_blank", style={'textDecoration': 'none', 'color':"#457b9d"})
+                                                    ], 
+                                                ),  
+                                                html.A(dmc.Avatar(src='/assets/header.jpg',size="xs",radius="lg"),
+                                                        href="https://abhinavk910.github.io/portfolio/",target="_blank",
+                                                ),
+                                                html.A(dmc.Avatar(DashIconify(icon="mdi:linkedin", width=15, color="#a8dadc"),
+                                                    size="xs",radius="xs"), href="http://www.linkedin.com/in/abhinavk910", target="_blank",
+                                                ),
+                                                html.A(dmc.Avatar(DashIconify(icon="mdi:github", width=15, color="#a8dadc"),#'#24292f'
+                                                       size="xs",radius="xs"), href="https://github.com/Abhinavk910/Data-Visualization/tree/main/apps/Makeover_Mondays",
+                                                       target="_blank",
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                ),
+                            ]
+                        ),
+                        html.Div(
+                            hidden=False,
+                            id='html-div-to-hide',
+                            children=[
+                                html.H1(
+                                    style={'fontSize':'4rem'},
+                                    children=[
+                                        'Model Competition',
+                                        html.Span(className='start'),
+                                        html.Span(className='reverse')
+                                    ],
+                                ),
+                                html.H1(
+                                    style={'fontSize':'.9rem', 'margin':'0px'},
+                                    children=[
+                                        'Director and Producer : Abhinav Kumar',
+                                        html.Span(className='start'),
+                                        html.Span(className='reverse')
+                                    ],
+                                )
+                            ]
+                        ),
+                        html.Div(
+                            id='html-div-to-hide2',
+                            hidden=True,
+                            style={'width':'95%'},
+                            children=[
+                                dmc.ActionIcon(DashIconify(icon='iconamoon:restart-fill'), id='back-to-start'),
+                                dmc.Center(
+                                    style={'flexWrap':'wrap'},
+                                    m=20,
+                                    children=[
+                                        dmc.Spoiler(
+                                            showLabel="Show more",
+                                            hideLabel="Hide",
+                                            maxHeight=35,
+                                            styles = {'root':{'fontSize':'10px'}},
+                                            children=[
+                                                dmc.Text(
+                                                    style={"maxWidth": 600}, weight=400, size=11, mb=10,  color="dimmed",
+                                                    children=[
+                                                    """
+                                                    Model Competition is an innovative application designed for comparing and evaluating LLM models.
+                                                    With this application, users can engage in friendly competitions between two or more LLM models
+                                                    to determine the most efficient and accurate model for a specific task. The process begins by
+                                                    selecting the number of competitors or models to participate in the competition. Next, you have 
+                                                    to ask series of questions or tasks and the models compete against each other to provide the 
+                                                    quickest response time. The model that consistently delivers the fastest responses is declared 
+                                                    the winner. Model Competition provides an interactive and dynamic platform for users to assess 
+                                                    the performance of LLM models and make informed decisions based on their speed and efficiency.
+                                                    """
+                                                    ]
+                                                ),
+
+                                            ],
+                                        ),
+                                    ]
+                                ),
+                                dmc.Accordion(
+                                    id='competition-accordion-p8',
+                                    style={'display':''},
+                                    mb=50,
+                                    children=[
+                                        dmc.AccordionItem(
+                                            value='competition-setup',
+                                            children=[
+                                                dmc.AccordionControl('Competition Setup'),
+                                                dmc.AccordionPanel(
+                                                    children=[
+                                                        dmc.NumberInput(
+                                                            id='competitor-count-p8',
+                                                            label='Competitors',
+                                                            description='Number of Models to Compete',
+                                                            min=2,
+                                                            max=5,
+                                                            step=1,
+                                                            value=2,
+                                                            icon=dmc.ActionIcon(DashIconify(icon='icon-park-outline:come'))
+                                                        ),
+                                                        html.Div(
+                                                            children=[
+                                                                dmc.Text(id='check'),
+                                                                dmc.Stack(
+                                                                    id='competitor-selection-p8',
+                                                                    mt=10,
+                                                                )
+                                                            ]
+                                                        )
+                                                    ]
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                dmc.Accordion(
+                                    id='competition-accordion-api-p8',
+                                    style={'display':''},
+                                    children=[]
+                                ),
+                                html.Div(
+                                    id='compete-page-p8',
+                                    style={'padding':'10px'},
+                                    hidden=True,
+                                    children=[
+                                        dmc.Group(
+                                            spacing=5,
+                                            children=[
+                                                dmc.ActionIcon(
+                                                    DashIconify(icon='ep:back'),
+                                                    id='back-question-p8',
+                                                    variant='outline',
+                                                    disabled=True
+                                                ),
+                                                dmc.ActionIcon(
+                                                    DashIconify(icon='ep:back', flip='horizontal'),
+                                                    id='next-question-arrow-p8',
+                                                    variant='outline',
+                                                    disabled=True
+                                                )
+                                            ]
+                                        ),
+                                        dmc.Textarea(
+                                            id='question-input-p8',
+                                            label='Ask Question',
+                                            my=20,
+                                        ),
+                                        dmc.ActionIcon(
+                                            DashIconify(icon='simple-icons:askubuntu', width=70),
+                                            my=20,
+                                            size='xl',
+                                            mx='auto',
+                                            variant='subtle',
+                                            id='answer-output-p8'
+                                        ),
+                                        dmc.Center(
+                                            dmc.Button("Next",
+                                                       id='next-btn-p8',
+                                                       leftIcon=DashIconify(icon='tabler:player-track-next'),
+                                                       my=20,
+                                                       style={'display':'none'},
+                                                       variant="gradient"),
+                                        ),
+                                        dmc.Divider(variant="dashed", mb=20),
+                                        dmc.Grid(
+                                            id='competitor-card-p8',
+                                            align='space_around',
+                                            justify='center'
+                                        ),
+                                        dmc.Divider(variant="dashed", m=50),
+                                        dmc.Alert(children=['Atleast ask one question.'],
+                                                          id='no-question-asked-p8',
+                                                          color='red',
+                                                          hide=True,
+                                                          duration=10000
+                                        ),
+                                        dmc.Center(
+                                            style={'marginTop':'20px'},
+                                            children=[
+
+                                                dmc.Button("Download xlsx", id="btn-xslx-p8",
+                                                       leftIcon=DashIconify(icon="material-symbols:download-rounded"),
+                                                       style={'width':'200px', 'margin':'auto'}),
+                                                dcc.Download(id="download-xslx-p8")
+                                            ]   
+                                        )
+                                        
+                                    ]
+                                )
+                            ]
+                        )
+                    ],
+                )
+            ]
+        )
+    ]
+)
